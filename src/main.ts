ebd1c940fab798d4fb0d36b5f3cf23366d56fdac
Ihunna Promise <pihunna@yahoo.com>
2024-07-04 22:31:36 +0100

    CORS configuration in main.ts file

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import * as path from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors();
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
 
